dd98b11878cea0512d1110f3d0b54d59e38b3fd0
mvtvn78 <mvtvn78@gmail.com>
2024-05-04 20:27:45 +0700

    Fun Fun

---
+++ Basic/master_loop.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() 
+{
+	// loop in C can be divided into 3 types : for loop, while loop,do_while loop
+	// for (intialization ; condition; updatecounter)
+	// while(condition)
+	// do {} while(condition)
+	//Task 1: print 100 time Love
+	//=============================================================================
+	
+//	for (int i =1 ; i<=100;++i)
+//	{
+//		cout<<i<<" Love"<<endl;
+//	}
+	
+	
+	//continue : this instruction can used to skip turn you want
+	//Task 2 : print number is odd
+	//=============================================================================
+	
+	
+//	for(int i= 100;i<= 200;++i)
+//	{
+//		if(i%2 ==0)
+//		{
+//			continue;
+//		}
+//		cout<<i<<endl;
+//	}
+	
+	//break : this instruction can used to exit the loop
+	//Task 3 : Print all number if number is square number you have to exit loop
+	//=============================================================================
+	
+	
+//	for(int i= 2;i<= 200;++i)
+//	{
+//		int square = sqrt(i);
+//		if(square*square ==i)
+//		{
+//			cout<<"Number is square number\nExit Loop\n";
+//			break;
+//		}
+//		cout<<i<<endl;
+//	}
+	
+	
+	
+	
+	return 0;	
+}
