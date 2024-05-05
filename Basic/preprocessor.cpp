309b779dea5ccde1cd9a7ca3f52b9f1421120ef7
mvtvn78 <mvtvn78@gmail.com>
2024-05-05 14:47:39 +0700

    PreProcessor

---
+++ Basic/preprocessor.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+/*preprocessor include : 
+	#define	: used to define a marco or constant
+	#undef: used to undefined a marco
+	#include : used to include a file in the source code program
+	#ifdef : check define
+	#ifndef : check define is not
+	#if : check condition 
+	#else :  excute when if fails
+	#endif : used to mark the end of #if , #ifdef and #ifndef
+*/
+#define LIMIT 5;
+#define MULT(a, b) (a * b)
+#define loop(i,c) for(int var = i; var<=c ;++var)
+int main()
+{
+	//Using marco
+	int l1 = 10, l2 = 5, area;
+    area = MULT(l1, l2);
+    cout<<area;
+    
+	// using forloop by marco
+    loop(2,4)
+    cout<<"Hello"<<endl;
+    
+    
+	return 0;
+}
