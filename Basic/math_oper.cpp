9f3bfd083937eced11e4106eb7696ba6f91ab373
mvtvn78 <mvtvn78@gmail.com>
2024-05-05 14:34:10 +0700

    Master Operator In C++ :>

---
+++ Basic/math_oper.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	// Math operator include : + , - , * , / , -- prefix , -- suffixes ,% Modulus
+	//int 
+	int a = 9;
+	int b = 10;
+	cout<<"Divides a by b "<< a/b<<endl; // OutPut : 0
+	cout<<"Divides a by b "<< 9/10<<endl; // OutPut : 0
+	//float
+	cout<<"Divides a by b "<< 9.0/10<<endl; // OutPut : 0.9
+	
+	//Remainder
+	cout<<"Remainder : "<<10%3 <<endl; // OutPut : 1
+	cout<<"Remainder : "<<10%5 <<endl; // OutPut : 0
+//	cout<<"Remainder : "<<10%0 <<endl; // OutPut : error
+	cout<<"Remainder : "<<100%100 <<endl; // OutPut : 0
+	cout<<"Remainder : "<<103%100 <<endl; // OutPut : 3
+	// statement : (a % b) if a is less than b , result will a.
+	cout<<"Remainder : "<<10%100 <<endl; // OutPut : 10
+	
+	// prefix and suffixes
+	
+	cout << a-- << b++ <<endl; // OutPut : 9 ,10
+	cout << a++ << b<<endl; // OutPut : 8, 11
+	cout << --a << ++b<<endl; // OutPut : 8 , 12
+	cout << a+b <<endl; // OutPut : 20
+	cout << a%b <<endl; // OutPut : 8
+	cout << a++%b<<endl; // OutPut : 8
+	
+	
+	return 0;
+}
