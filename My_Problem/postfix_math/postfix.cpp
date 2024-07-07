235e1e6109a3c5ed17e6140c2d90c89ac40e5669
mvtvn78 <mvtvn78@gmail.com>
2024-07-07 18:54:49 +0700

    Do calculation with string mathmetical calculation

--- My_Problem/postfix_math/postfix.cpp
+++ My_Problem/postfix_math/postfix.cpp
@@ -3,7 +3,6 @@
 #include <queue>
 using namespace std;
 /*
-
  .----------------.  .----------------.  .----------------. 
 | .--------------. || .--------------. || .--------------. |
 | | ____    ____ | || | ____   ____  | || |  _________   | |
@@ -15,8 +14,6 @@ using namespace std;
 | |              | || |              | || |              | |
 | '--------------' || '--------------' || '--------------' |
  '----------------'  '----------------'  '----------------' 
-
-
 */
 
 //Precendence Method
@@ -30,10 +27,13 @@ int precedence(char x)
 		return 2;
 	return 3;
 }
+// isOperand Method
 bool isOperand(char c)
 {
+	// get by Unicode : 0-9 
 	return c >=48 && c<=58;
 }
+// getPostFix Method
 string getPostFix(string infix)
 {
 	string result = "";
@@ -47,12 +47,14 @@ string getPostFix(string infix)
 		{
 			char e_cur = infix[i];
 			if(stack.empty()) 
-				stack.push(e_cur);
+				stack.push(e_cur); // empty push it into stack
 			else
 			{
+				// get top element in stack
 				char e_top = stack.top();
 				if (e_cur == ')')
 				{
+					// loop until top element is '('
 					while(stack.top() != '(')
 					{
 						queue.push(stack.top());
@@ -69,9 +71,11 @@ string getPostFix(string infix)
 				}
 				else
 				{
+					//  current element priority less than  top elmenet in stack
+					// move top element in stack to queue and add current element to stack
 					queue.push(e_top);
 					stack.pop();
-					stack.push(e_cur);
+					stack.push(e_cur); 
 				}
 			}
 		}
@@ -92,16 +96,54 @@ string getPostFix(string infix)
 	}
 	return result;
 }
-void getUnicode(char c)
+// stringCalculation Method
+int stringCalculation(string postfix)
 {
-	cout<< int(c)<<endl;
+	stack<int> stack;
+	for(int i = 0 ;i<postfix.size();++i)
+	{
+		char curr_element = postfix[i];
+		if(isOperand(curr_element))
+		{
+			int num = curr_element - '0'; // convert char to int 
+			stack.push(num);
+		}
+		else 
+		{
+			int b= stack.top(); // get top element
+			stack.pop();
+			int a= stack.top(); // get top element 
+			stack.pop();
+			int temp = 0;
+			// switch to correct perform operation
+			switch(curr_element)
+			{
+				case '+': 
+					temp = a +b; 
+				break;
+				case '-': 
+					temp = a -b;
+				break;
+				case '*': 
+					temp = a *b;
+				break;
+				case '/': 
+					temp = a /b;
+				break;
+			}
+			stack.push(temp); // push result to top element in stack		
+		}
+	}
+	return stack.top(); // result is top elmenet in stack
 }
 int main()
 {
-	///5*(7-4)+4*(5-3) : operand <10
+	///5*(7-4)+4*(5-3) : operand <10 and natural number
 	string infix;
 	cout << "Type your infix: ";
 	cin >> infix;
-	cout<< "Your Postfix :"<<getPostFix(infix);
+	string postfix =getPostFix(infix);
+	cout<< "Your Postfix : "<< postfix<<endl;
+	cout<< "Your Result : "<<stringCalculation(postfix);
 	return 0;
 }
